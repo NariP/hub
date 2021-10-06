299ccdc7c3cd1cc971151ef2fd7896aa5bcb4782
박나리 <>
2021-10-06 17:45:04 +0900

    feature: Observable 클래스화, 코드 수정 #12

--- client/src/pages/Main/MainPage.ts
+++ client/src/pages/Main/MainPage.ts
@@ -1,30 +1,76 @@
 import Component from '@/core/Component';
-import { observe, observable } from '@/core/observer';
+import { observe, observable, Observable } from '@/core/observer';
+import Buttons from '@/Buttons';
 
 export default class MainPage extends Component {
+  state: { count: number } | undefined;
+  setup() {
+    super.setup();
+    this.state = { count: 0 };
+    console.log(this.state);
+  }
+
   template(): string {
     super.template();
-    return `<h1>MainPage</h1>`;
+    return `<h1>count: ${this.state?.count}</h1><div data-component='test-fake-redux'></div>`;
+  }
+  increase() {
+    if (!this.state?.count && this.state?.count !== 0) return;
+    this.setState({ count: this.state?.count + 1 });
+  }
+  decrease() {
+    if (!this.state?.count && this.state?.count !== 0) return;
+    this.setState({ count: this.state?.count - 1 });
   }
   update() {
     super.update();
-    const testState = { a: 2, b: 1 };
-    const proxyState = observable(testState) as { [key: string]: any };
+    // const testState = { a: 2, b: 1 };
+    // const proxyState = observable(testState) as { [key: string]: any };
+    //
+    // // observableTest
+    //
+    // const adder = () => {
+    //   console.log(`adder: ${proxyState.a + proxyState.b}`);
+    // };
+    // const minus = () => {
+    //   console.log(`minus; ${proxyState.a - proxyState.b}`);
+    // };
+    //
+    // observe(adder);
+    // observe(minus);
+    //
+    // proxyState.a = 3;
+    // console.log(proxyState);
 
-    // observableTest
+    const $mainPage: HTMLElement | null | undefined =
+      this.$target?.querySelector('[data-component="test-fake-redux"]');
+    if (!$mainPage) return;
+    const { increase, decrease, state } = this;
+    new Buttons($mainPage, {
+      increase: increase.bind(this),
+      decrease: decrease.bind(this),
+    });
 
-    const adder = () => {
-      console.log(`adder: ${proxyState.a + proxyState.b}`);
+    type dynamic = {
+      [key: string]: any;
+    };
+    const testObject: dynamic = { a: 'a' };
+
+    const subscriber = new Observable(testObject) as dynamic;
+    const add = () => {
+      console.log(`${subscriber.get().a} + 1 = ${subscriber.get().a + 1}`);
     };
     const minus = () => {
-      console.log(`minus; ${proxyState.a - proxyState.b}`);
+      subscriber.get().a = 2;
+      console.log(`${subscriber.get().a} - 1 = ${subscriber.get().a - 1}`);
     };
+    subscriber.observe(add);
+    // subscriber.observe(minus);
 
-    observe(adder);
-    observe(minus);
-
-    proxyState.a = 3;
-    console.log(proxyState);
+    subscriber.get().a = 3;
+    console.log(subscriber.get());
+    subscriber.get().a = 3;
+    // console.log(subscriber.get());
   }
 }
 
